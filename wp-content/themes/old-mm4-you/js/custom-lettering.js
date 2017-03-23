a4fab1267f04fa12cf4a5e317e46e65131d51815
Mox Biggs <moxdev1@gmail.com>
2017-03-23 11:15:19 -0400

    pulled from live site

---
+++ wp-content/themes/old-mm4-you/js/custom-lettering.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function() {
+    jQuery(".why-nav-link a").lettering('words');
+    jQuery(".contracts-nav-link a").lettering('words');
+    jQuery(".about-nav-link a").lettering('words');
+    jQuery("#home-highlight-inner-wrapper  > .home-highlight:last-child > .highlight-title").lettering('words');
+    jQuery(".page-id-1816 #secondary #hot-jobs-sidebar #hot-jobs-inner-wrapper .hot-jobs-highlight:last-child .hot-jobs-title").lettering('words');
+    jQuery(".page-id-1816 #secondary #custom-sidebar-button .sidebar-btn a").lettering('words');
+    jQuery(".page-id-1818 #secondary #custom-sidebar-button .sidebar-btn a").lettering('words');
+    jQuery(".page-id-1820 #secondary #custom-sidebar-button .sidebar-btn a").lettering('words');
+    jQuery(".page-id-1896 #secondary #custom-sidebar-button .sidebar-btn a").lettering('words');
+    jQuery(".page-id-1814 .entry-header h1").lettering('words');
+    jQuery(".page-id-1863 .entry-header h1").lettering('words');
+    jQuery(".page-id-2139 .entry-header h1").lettering('words');
+    jQuery(".page-id-1816 .entry-header h1").lettering('words');
+    jQuery(".page-id-1818 .entry-header h1").lettering('words');
+    jQuery(".page-id-1820 .entry-header h1").lettering('words');
+    jQuery(".page-id-1896 .entry-header h1").lettering('words');
+});
